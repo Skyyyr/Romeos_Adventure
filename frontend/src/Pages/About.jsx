b02fabba7340fdcdacf48dcc72bf2d1b1abebbda
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-08-24 17:04:15 -0400

    about page icons

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,13 +1,25 @@
 import githubicon from "../features/assets/github.png"
 
 
+
 function About() {
 
     return (
-        <div>
+        <div className="row">
+        <div className="col-5"></div>
+        <div className="col-4 align-self-center" style={{'text-align': 'left'}}>
+         <div style={{'text-align': 'center'}}>  
             <br></br>
             <h2 style={{ fontWeight: 'bold' }}>Meet the Team</h2>
             <br></br>
+            <div className="row text-align-center" >
+                <div id="skyler-icon"></div>
+                <div id="justin-icon"></div>
+                <div id="garrett-icon"></div>
+                <div id="meredith-icon"></div>
+                <div id="zack-icon"></div>
+            </div>
+            </div> 
             <div >
             <p style={{ fontWeight: 'bold' }}>Skyler Scott <img class='github-icon' src={githubicon}/><a href="https://github.com/Skyyyr" target="_blank" rel="noreferrer">
             /Skyyyr </a></p>
@@ -33,13 +45,14 @@ function About() {
             </div>
             
             <div>
-            <p style={{ fontWeight: 'bold' }}>Zach Fair <img class='github-icon' src={githubicon}/><a href="https://github.com/zaxx81" target="_blank" rel="noreferrer">
+            <p style={{ fontWeight: 'bold' }}>Zack Fair <img class='github-icon' src={githubicon}/><a href="https://github.com/zaxx81" target="_blank" rel="noreferrer">
             /zaxx81</a></p>
             
             </div>
             <br></br>
             <br></br>
            
+        </div>
         </div>
 
 
