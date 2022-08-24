973bd7568624874630bf92566f1ce7db89f3d191
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-08-24 13:22:00 -0400

    about page

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,12 +1,48 @@
+import githubicon from "../features/assets/github.png"
+
+
 function About() {
 
     return (
         <div>
-            <h1>
-                About Page
-                <h1>MY NAME IS MEREDITH</h1>
-            </h1>
+            <br></br>
+            <h2 style={{ fontWeight: 'bold' }}>Meet the Team</h2>
+            <br></br>
+            <div >
+            <p style={{ fontWeight: 'bold' }}>Skyler Scott <img class='github-icon' src={githubicon}/><a href="https://github.com/Skyyyr" target="_blank" rel="noreferrer">
+            /Skyyyr </a></p>
+            
+            </div>
+            
+            <div>
+            <p style={{ fontWeight: 'bold' }}>Justin Peterson <img class='github-icon' src={githubicon}/> <a href="https://github.com/jjpeterson90" target="_blank" rel="noreferrer">
+            /jjpeterson90 </a></p>
+            
+            </div>
+            
+            <div>
+            <p style={{ fontWeight: 'bold' }}>Garrett Adams <img class='github-icon' src={githubicon}/><a href="https://github.com/garrettadams52" target="_blank" rel="noreferrer">
+            /garrettadams52 </a></p>
+            
+            </div>
+            
+            <div>
+            <p style={{ fontWeight: 'bold' }}>Meredith Hall  <img class='github-icon' src={githubicon}/><a href="https://github.com/meredithhall716" target="_blank" rel="noreferrer">
+            /meredithhall716</a></p>
+            
+            </div>
+            
+            <div>
+            <p style={{ fontWeight: 'bold' }}>Zach Fair <img class='github-icon' src={githubicon}/><a href="https://github.com/zaxx81" target="_blank" rel="noreferrer">
+            /zaxx81</a></p>
+            
+            </div>
+            <br></br>
+            <br></br>
+           
         </div>
+
+
     )
 }
 
