bea5030356e2b48a1a5af9304b513be437509b3a
Justin <jjpeterson90@hotmail.com>
2022-08-25 18:48:23 -0500

    battle animation changes - incomplete

--- frontend/src/features/battling/components/BattleView.jsx
+++ frontend/src/features/battling/components/BattleView.jsx
@@ -3,104 +3,36 @@ import {Bar} from './HealthBar'
 import {getCharacterData} from '../../character/data/characterdata'
 import {getEnemyData} from '../../character/data/enemydata'
 import { useState, useEffect, useRef } from 'react'
-import  Button from "@mui/material/Button" 
+import Button from "@mui/material/Button" 
 import { Typography, Tooltip } from '@mui/material'
 import {wait, Damage} from '../helpers/helpers'
 
-import CanvasWalk from '../helpers/animations'
-
+import Animation from '../helpers/Animation'
+import { walkRight, walkLeft, getAttackAnimation } from '../helpers/AnimationConstants'
 
 function BattleView({gameData, enemy, setGameMode,nextStage}) {
     
-    const [romeoStats,setRomeoStats] = useState({'accuracy':gameData.accuracy,'defense':gameData.defense,'evasion':gameData.evasion,'strength':gameData.strength})
-    const [romeoMoves,setRomeoMoves] = useState(getCharacterData(gameData.type).MOVES)
-    const [enemyData,setEnemyData] = useState(getEnemyData(enemy))
+    const [romeoStats, setRomeoStats] = useState({'accuracy':gameData.accuracy,'defense':gameData.defense,'evasion':gameData.evasion,'strength':gameData.strength})
+    const [romeoMoves, setRomeoMoves] = useState(getCharacterData(gameData.type).MOVES)
+    const [enemyData, setEnemyData] = useState(getEnemyData(enemy))
     const [turn, setTurn] = useState('Player One')
     const [romeoHealth, setRomeoHealth] = useState(100)
     const [enemyHealth, setEnemyHealth] = useState(100)
     const [currAttack, setCurrAttack] = useState('')
-    const [enemyAnimation, setEnemyAnimation] = useState('')
-    const [playerAnimation, setPlayerAnimation] = useState('')
 
-    const [ moveAnimation, setMoveAnimation ] = useState('')
-    const [ flip, setFlip ] = useState(false)
+    const [playerEffect, setPlayerEffect] = useState('')
+    const [enemyEffect, setEnemyEffect] = useState('')
+    
+    const [enemyAnimation, setEnemyAnimation] = useState('')
+    const [enemyMoveEffect, setEnemyMoveEffect] = useState('')
+    const [enemyFlip, setEnemyFlip] = useState('')
+
+    const [playerAnimation, setPlayerAnimation] = useState('')
+    const [playerMoveEffect, setPlayerMoveEffect] = useState('')
+    const [playerFlip, setPlayerFlip] = useState('')
 
     const newKey = useRef(0)
-
-    // walk right: -705px, 64x64, 9 frames
-    // big atk right: -1921px, 192x192, 6 frames
-    // big atk left: -1537px, 192x192, 6 frames
-
-    const walkRight = {
-      oversize: false,
-      row: 12,
-      frames: 9,
-      repeat: true,
-      type: 'walk',
-    }
-    const lgAtkRight = {
-      oversize: true,
-      row: 11,
-      frames: 6,
-      repeat: false,
-      type: 'atk',
-    }
-    const walkLeft = {
-      oversize: false,
-      row: 10,
-      frames: 9,
-      repeat: true,
-      type: 'walk',
-    }
-    const lgAtkLeft = {
-      oversize: true,
-      row: 9,
-      frames: 6,
-      repeat: false,
-      type: 'atk',
-    }
     
-
-    const enemyAttack = async (move) => {
-        await wait(3000)
-        // setEnemyAnimation('eneAttack')
-        // await wait(500)
-        // setEnemyAnimation("eneReset")
-        // setPlayerAnimation('hit')
-        // await wait(500)
-        // setPlayerAnimation('')
-        // inflictDamage(move)
-        setTurn("Player One")
-    }
-
-    const attack = async (move) => {
-        document.getElementById(move.name).disabled = true;
-        setMoveAnimation('user-walk')
-        newKey.current++
-        setPlayerAnimation(walkRight)
-        await wait(2200)
-        newKey.current++
-        setPlayerAnimation(lgAtkRight)
-        await wait(600)
-        inflictDamage(move)
-        newKey.current++
-        setFlip('flip-sprite')
-        setPlayerAnimation(walkLeft)
-        await wait(2200)
-        newKey.current++
-        setMoveAnimation('')
-        setPlayerAnimation('')
-        setFlip('')
-        // setPlayerAnimation('attack')
-        // await wait(500)
-        // setPlayerAnimation("reset")
-        // setEnemyAnimation('hit')
-        // await wait(500)
-        // inflictDamage(move)
-        // setEnemyAnimation('')
-        // setTurn("Player Two")
-        document.getElementById(move.name).disabled = false;
-    }
 
     useEffect(()=>{
         if(turn === "Player Two"){
@@ -112,17 +44,33 @@ function BattleView({gameData, enemy, setGameMode,nextStage}) {
     useEffect(()=>{
         const checkDead = async () =>{
             if(romeoHealth <= 0){
-                await wait(2000)
+                await wait(3000)
                 setGameMode('BattleEndLost')
             }
             else if(enemyHealth <= 0){
-                await wait(2000)
+                await wait(3000)
                 setGameMode('BattleEndWon')
                 nextStage()
             }
         }
         checkDead()
     },[romeoHealth,enemyHealth])
+    
+    const attack = async(move) => {
+        document.getElementById(move.name).disabled = true;
+        const atkAnim = getAttackAnimation(move.label)
+        if (move.type === 'melee') playerMelee(move, atkAnim)
+        if (move.type === 'ranged') playerRanged(move, atkAnim)
+        if (move.type === 'magic') playerMagic(move, atkAnim)
+    }
+
+    const enemyAttack = async(move) => {
+        await wait(1000)
+        const atkAnim = getAttackAnimation(move.label)
+        if (move.type === 'melee') enemyMelee(move, atkAnim)
+        if (move.type === 'ranged') enemyRanged(move, atkAnim)
+        if (move.type === 'magic') enemyMagic(move, atkAnim)
+    }
 
     const inflictDamage = async (move) => {
         const damage = Damage(turn, romeoStats, enemyData.STATS, move)
@@ -136,6 +84,124 @@ function BattleView({gameData, enemy, setGameMode,nextStage}) {
         }
     }
 
+    function reverseMagic(anim) {
+      let revFrames = anim.frames.reverse()
+      anim.frames = [...revFrames]
+      console.log('reversed?', anim)
+      return anim
+    }
+
+    async function playerMelee(move, atkAnim) {
+      setPlayerMoveEffect('user-walk')
+      newKey.current++
+      setPlayerAnimation(walkRight)
+      await wait(2000)
+      newKey.current++
+      setPlayerAnimation(atkAnim)
+      await wait(500)
+      // setEnemyEffect('hit')
+      inflictDamage(move)
+      await wait(500)
+      // setEnemyEffect('')
+      newKey.current++
+      setPlayerFlip('flip')
+      setPlayerAnimation(walkLeft)
+      await wait(2000)
+      newKey.current++
+      setPlayerMoveEffect('')
+      setPlayerAnimation('')
+      setPlayerFlip('')
+      await wait(1000)
+      document.getElementById(move.name).disabled = false;
+      setTurn("Player Two")
+    }
+    async function playerRanged(move, atkAnim) {
+      newKey.current++
+      setPlayerAnimation(atkAnim)
+      await wait(1000)
+      // setEnemyEffect('hit')
+      inflictDamage(move)
+      await wait(1000)
+      // setEnemyEffect('')
+      newKey.current++
+      setPlayerAnimation('')
+      await wait(1000)
+      document.getElementById(move.name).disabled = false;
+      setTurn("Player Two")
+    }
+    async function playerMagic(move, atkAnim) {
+      setPlayerMoveEffect('magic-hover')
+      newKey.current++
+      setPlayerAnimation(atkAnim)
+      await wait(1500)
+      // setEnemyEffect('hit')
+      inflictDamage(move)
+      newKey.current++
+      setPlayerAnimation(reverseMagic(atkAnim))
+      await wait(1500)
+      // setEnemyEffect('')
+      newKey.current++
+      setPlayerAnimation('')
+      await wait(500)
+      setPlayerMoveEffect('')
+      document.getElementById(move.name).disabled = false;
+      setTurn("Player Two")
+    }
+
+    async function enemyMelee(move, atkAnim) {
+      setEnemyMoveEffect('enemy-walk')
+      newKey.current++
+      setEnemyAnimation(walkLeft)
+      await wait(2000)
+      newKey.current++
+      setEnemyAnimation(atkAnim)
+      await wait(500)
+      // setPlayerEffect('hit')
+      inflictDamage(move)
+      await wait(500)
+      // setPlayerEffect('')
+      newKey.current++
+      setEnemyFlip('flip')
+      setEnemyAnimation(walkRight)
+      await wait(2000)
+      newKey.current++
+      setEnemyMoveEffect('')
+      setEnemyAnimation('')
+      setEnemyFlip('')
+      await wait(1000)
+      setTurn("Player One")
+    }
+    async function enemyRanged(move, atkAnim) {
+      newKey.current++
+      setEnemyAnimation(atkAnim)
+      await wait(1000)
+      // setPlayerEffect('hit')
+      inflictDamage(move)
+      await wait(1000)
+      // setPlayerEffect('')
+      newKey.current++
+      setEnemyAnimation('')
+      await wait (1000)
+      setTurn("Player One")
+    }
+    async function enemyMagic(move, atkAnim) {
+      setEnemyMoveEffect('magic-hover')
+      newKey.current++
+      setEnemyAnimation(atkAnim)
+      await wait(1500)
+      // setPlayerEffect('hit')
+      inflictDamage(move)
+      newKey.current++
+      setEnemyAnimation(reverseMagic(atkAnim))
+      await wait(1500)
+      // setPlayerEffect('')
+      newKey.current++
+      setEnemyAnimation('')
+      await wait(500)
+      setEnemyMoveEffect('')
+      await wait(1000)
+      setTurn("Player One")
+    }
 
     return (
         <>
@@ -148,19 +214,21 @@ function BattleView({gameData, enemy, setGameMode,nextStage}) {
                     <Bar label={enemyData.NAME} value={enemyHealth}/>
                 </div> 
             </div>
-            {/*<div className='row align-items-end' style={{'height':'20vh', 'overflow': 'hidden'}}>*/}
             <div className='row m-0 align-items-end' style={{'height':'40%'}}>
                 <div className='wrapper'>
-                    <div className = {"row align-items-center justify-content-center"} style={{'width':"300px", 'height': "200px"}}>
-                        <div id='canvas-container' className={`${moveAnimation} ${flip.current}`}>
+                    <div className ="row align-items-center justify-content-center" style={{'width':"300px", 'height': "200px"}}>
+                        <div id='canvas-container' className={`${playerMoveEffect} ${playerFlip}`}>
                           { playerAnimation ? 
-                              <CanvasWalk key={newKey.current} {...playerAnimation} />
-                            : <div className='frontend-battle'></div>}
+                              <Animation key={newKey.current} {...playerAnimation} type={gameData.type}/>
+                            : <div className={`${gameData.type}-battle player-battle-stance`}></div> }
                         </div>
-
                     </div>
                     <div className = {"row align-items-center justify-content-center"} style={{'width':"300px", 'height': "200px"}}>
-                        <div className={`${enemyData.NAME}-battle ${enemyAnimation}`}></div> 
+                        <div id='canvas-container' className={`${enemyMoveEffect} ${enemyFlip}`}>
+                          { enemyAnimation ?
+                              <Animation key={newKey.current} {...enemyAnimation} type={enemyData.NAME}/>
+                            : <div className={`${enemyData.NAME}-battle enemy-battle-stance`}></div> }
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ frontend/src/features/battling/helpers/AnimationConstants.jsx
@@ -0,0 +1,102 @@
+/* MOVEMENT */
+
+export const walkRight = {
+  height: 64,
+  width: 64,
+  row: 12,
+  frames: [0,1,2,3,4,5,6,7,8],
+  repeat: true,  
+}
+export const walkLeft = {
+  height: 64,
+  width: 64,
+  row: 10,
+  frames: [0,1,2,3,4,5,6,7,8],
+  repeat: true,
+}
+
+
+export function getAttackAnimation(type) {
+  switch (type) {
+    case 'pierceRight':
+      return ({
+        height: 64,
+        width: 64,
+        row: 8,
+        frames: [0,1,2,3,4,5,6,7],
+        repeat: false,
+      })
+    case 'bashRight:':
+      return ({
+        height: 64,
+        width: 64,
+        row: 16,
+        frames: [0,1,2,3,4,5],
+        repeat: false,
+      })
+    case 'slashRight':
+      return ({
+        height: 192,
+        width: 192,
+        row: 11,
+        frames: [0,1,2,3,4,5,0],
+        repeat: false,
+      })
+    case 'rngAtkRight':
+      return ({
+        height: 64,
+        width: 64,
+        row: 20,
+        frames: [0,1,2,3,4,5,6,7,8,9,10,11],
+        repeat: false,
+      })
+    case 'magAtkRight':
+      return ({
+        height: 64,
+        width: 64,
+        row: 4,
+        frames: [0,1,2,3,4,5],
+        repeat: false,
+      })
+    case 'pierceLeft':
+      return ({
+        height: 64,
+        width: 64,
+        row: 6,
+        frames: [0,1,2,3,4,5,6,7],
+        repeat: false,
+      })
+    case 'bashLeft:':
+      return ({
+        height: 64,
+        width: 64,
+        row: 14,
+        frames: [0,1,2,3,4,5],
+        repeat: false,
+      })
+    case 'slashLeft':
+      return ({
+        height: 192,
+        width: 192,
+        row: 9,
+        frames: [0,1,2,3,4,5,0],
+        repeat: false,
+      })
+    case 'rngAtkLeft':
+      return ({
+        height: 64,
+        width: 64,
+        row: 18,
+        frames: [0,1,2,3,4,5,6,7,8,9,10,11],
+        repeat: false,
+      })
+    case 'magAtkLeft':
+      return ({
+        height: 64,
+        width: 64,
+        row: 2,
+        frames: [0,1,2,3,4,5],
+        repeat: false,
+      })
+  }
+}
